1e9c91ca7ebf2b16d935e44bab1f17cd48324f54
Zaki <krait71094@gmail.com>
2018-09-22 23:18:37 +0800

    User section working except the security part

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -13,6 +13,7 @@
         <thead>
         <tr>
             <th>ID</th>
+            <th>Photo</th>
             <th>Name</th>
             <th>Email</th>
             <th>Role</th>
@@ -26,7 +27,8 @@
             @foreach($users as $user)
                 <tr>
                     <td>{{$user->id}}</td>
-                    <td>{{$user->name}}</td>
+                    <td><img height="50" src="{{$user->photo ? $user->photo->file : '/images/no_image_available.png'}}" alt=""></td>
+                    <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</a></td>
                     <td>{{$user->email}}</td>
                     <td>{{$user->role['name']}}</td>
                     <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
